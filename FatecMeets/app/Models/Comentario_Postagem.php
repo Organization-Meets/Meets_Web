a7080dd911f76e0f5377543859c5e4bce5c2329e
Gabriel da Silva Rodrigues <gabriel.rodrigues106@fatec.sp.gov.br>
2025-08-18 15:30:53 +0000

    refatora: Adiciona novos controladores e modelos para Academicos, Adicionais, Administradores, ChatDenunciado, Comentarios, Eventos, Gameficacao, Lixo, Logradouro, Lugares, Mensagens e Postagens

---
+++ FatecMeets/app/Models/Comentario_Postagem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comentario_Postagem extends Model
+{
+    use HasFactory;
+}

fee5588510cd56bfe043dd6ed5154d9b52e4c6fa
GabrielSilvaRodrigues1 <gabrielsilvagithub@gmail.com>
2025-08-20 18:48:50 +0000

    arrumando o routes e views para melhor organização

--- FatecMeets/resources/views/usuario/create.blade.php
+++ FatecMeets/resources/views/usuario/create.blade.php
@@ -50,3 +50,39 @@
     </script>
 </body>
 </html>
+
+<!--quando terminar o cadastro, mostrar:-->
+<link rel="stylesheet" href="/css/estilo-cadastro.css">
+<div class="container">
+    <h2>Tipo de Usuário</h2>
+
+    <form action="/usuarios/adicionais/{{ $usuario_id }}" method="POST">
+        @csrf
+        <img src="/imagens/logo.png" alt="Logo" style="width: 100px; margin-bottom: 15px; display: block; margin-left: auto; margin-right: auto;">
+
+        <p style="text-align: center; margin-bottom: 20px;">Tipo de Usuario:</p>
+        <select name="tipo_usuario" required>
+            <option value="{{ $tipo }}">{{ $tipo }}</option>
+        </select>
+
+        @if($tipo == 'aluno')
+            <input type="number" name="ra_aluno" placeholder="ra_aluno" required>
+            <input id="nome" type="text" name="nome_aluno" placeholder="Primeiro e último nome" readonly>
+        @elseif($tipo == 'professor')
+            <input type="number" name="ra_academicos" placeholder="ra_academicos" required>
+            <input id="nome" type="text" name="nome_academicos" placeholder="Primeiro e último nome" readonly>
+        @endif
+
+        <input type="text" name="nickname" placeholder="Nome de usuário" readonly>
+
+        <button type="submit">Continuar</button>
+    </form>
+
+    <a href="/home/"><button type="button">Voltar</button></a>
+</div>
+<script>
+    var nomeBackend = "{{ $nome }}";
+    var nicknameBackend = "{{ $nickname }}";
+</script>
+<script src="/js/autocompletarNome.js"></script>
+<script src="/js/autocompletarNickname.js"></script>
--- FatecMeets/routes/includes/UsuarioRoutes.php
+++ FatecMeets/routes/includes/UsuarioRoutes.php
@@ -3,12 +3,14 @@ use App\Http\Controllers\UsuarioController;
 Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
 Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
 Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
-Route::get('/usuarios/{id_usuario}', [UsuarioController::class, 'show'])->name('usuarios.show');
-Route::get('/usuarios/{id_usuario}/edit', [UsuarioController::class, 'edit'])->name('usuarios.edit');
-Route::put('/usuarios/{id_usuario}', [UsuarioController::class, 'update'])->name('usuarios.update');
-Route::delete('/usuarios/{id_usuario}', [UsuarioController::class, 'destroy'])->name('usuarios.destroy');
+Route::get('/usuarios/{id_usuario}', [UsuarioController::class, 'show'])
+    ->where('id_usuario', '[0-9]+')
+    ->name('usuarios.show');
+Route::get('/usuarios/{id_usuario}/edit', [UsuarioController::class, 'edit'])->where('id_usuario', '[0-9]+')->name('usuarios.edit');
+Route::put('/usuarios/{id_usuario}', [UsuarioController::class, 'update'])->where('id_usuario', '[0-9]+')->name('usuarios.update');
+Route::delete('/usuarios/{id_usuario}', [UsuarioController::class, 'destroy'])->where('id_usuario', '[0-9]+')->name('usuarios.destroy');
 Route::get('/usuarios/perfil', [UsuarioController::class, 'perfil'])->name('usuarios.perfil');
 Route::get('/usuarios/logout', [UsuarioController::class, 'logout'])->name('usuarios.logout');
-Route::get('/usuarios/loginForm', [UsuarioController::class, 'loginForm'])->name('usuarios.loginForm');
-Route::post('/usuarios/login', [UsuarioController::class, 'login'])->name('usuarios.login');
+Route::get('/usuarios/loginForm', [UsuarioController::class, 'loginForm'])->name('usuario.loginForm');
+Route::post('/usuarios/login', [UsuarioController::class, 'login'])->name('usuario.login');
 ?>

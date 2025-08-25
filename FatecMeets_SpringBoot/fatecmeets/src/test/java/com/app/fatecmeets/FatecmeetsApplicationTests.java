9b24d6cd3aebdd0519b79b0f14b6cd05b629c01c
GabrielSilvaRodrigues1 <gabrielsilvagithub@gmail.com>
2025-08-25 11:04:30 +0000

    FatecMeets SpringBoot

---
+++ FatecMeets_SpringBoot/fatecmeets/src/test/java/com/app/fatecmeets/FatecmeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fatecmeets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecmeetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

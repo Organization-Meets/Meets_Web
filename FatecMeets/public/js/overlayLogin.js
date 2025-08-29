2af77ba61fc46eddce5eecae5632e3a789ae0bed
Gabriel da Silva Rodrigues <gabriel.rodrigues106@fatec.sp.gov.br>
2025-08-29 15:58:29 -0300

    Atualizar o overlayLogin.js

--- FatecMeets/public/js/overlayLogin.js
+++ FatecMeets/public/js/overlayLogin.js
@@ -1,22 +1,4 @@
 
-</head>
-<body>
-
-<div class="conteudo">
-    <button id="abrirLogin">Abrir Login</button>
-</div>
-
-<div class="login-overlay" id="loginOverlay">
-    <div class="form-login">
-        <h2>Login</h2>
-        <form>
-            <input type="text" placeholder="Usuário" required><br>
-            <input type="password" placeholder="Senha" required><br>
-            <button type="submit">Entrar</button>
-        </form>
-    </div>
-</div>
-
 <script>
     const abrirLogin = document.getElementById('abrirLogin');
     const loginOverlay = document.getElementById('loginOverlay');

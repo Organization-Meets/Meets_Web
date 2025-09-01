9a6402d44866490a5f5c6694f68a4bca8471a7da
GabrielSilvaRodrigues2 <gabrielsilvagithub2@gmail.com>
2025-09-01 20:14:33 +0000

    esqueci de importar: import org.springframework.beans.factory.annotation.Value;

--- FatecMeetsSpringBoot/backend/src/main/java/com/fatecmeets/backend/auth/EmailService.java
+++ FatecMeetsSpringBoot/backend/src/main/java/com/fatecmeets/backend/auth/EmailService.java
@@ -3,6 +3,7 @@ package com.fatecmeets.backend.auth;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class EmailService {
